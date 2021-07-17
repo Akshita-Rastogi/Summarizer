f0aed03b1af8d0062424fddb061e618c22398770
Akshita-Rastogi <65523112+Akshita-Rastogi@users.noreply.github.com>
2021-07-17 22:07:24 +0530

    Update settings.py

--- AutomaticTextSummarizer/settings.py
+++ AutomaticTextSummarizer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,17 +85,16 @@ WSGI_APPLICATION = 'AutomaticTextSummarizer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': '',
         'NAME': 'text_summarizer',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '',
         'PORT': '',
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
