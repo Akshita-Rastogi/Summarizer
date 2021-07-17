835c96d4be21557b41747a58af87d0cbd8b9c3e8
Akshita-Rastogi <65523112+Akshita-Rastogi@users.noreply.github.com>
2021-07-17 22:06:09 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
             "available on your PYTHON PATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    #execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
